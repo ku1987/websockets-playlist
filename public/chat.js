17f44e4bc31675f254dc14e76db7752f5a934fdf
ku1987 <kei.usami1987@gmail.com>
2020-08-22 18:28:50 +0900

    Add events for chatting and broadcasting

--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,30 @@
 // Make connection
 const socket = io.connect('http://localhost:4000');
+
+const message = document.getElementById('message');
+const handle = document.getElementById('handle');
+const btn = document.getElementById('send');
+const output = document.getElementById('output');
+const feedback = document.getElementById('feedback')
+
+// Emit events
+btn.addEventListener('click', () => {
+  socket.emit('chat', {
+    message: message.value,
+    handle: handle.value,
+  });
+});
+
+message.addEventListener('keypress', () => {
+  socket.emit('typing', handle.value);
+});
+
+// Listen for events
+socket.on('chat', (data) => {
+  output.innerHTML += `<p><strong>${data.handle}:</strong>${data.message}</p>`;
+  feedback.innerHTML = '';
+});
+
+socket.on('typing', (data) => {
+  feedback.innerHTML = `<p><em>${data} is typing a message...</em></p>`;
+});
